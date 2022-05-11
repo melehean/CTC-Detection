b048afd74515413cd72ec308a4795055b8be02e2
Laura <laurazembrzuska@gmail.com>
2022-05-12 00:46:12 +0200

    Added heatmap

---
+++ heatmap.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+class Heatmap:
+    def __init__(self, train_data):
+        self.train_data = train_data
+
+    def heatmap(self):
+        matrix = self.train_data.corr().round(2)
+        mask = np.triu(np.ones_like(matrix, dtype=bool))
+        sns.clustermap(matrix, annot=True, vmax=1, vmin=-1, center=0, cmap='vlag', mask='mask')
+        plt.show()
