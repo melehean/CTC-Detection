6391f3750567400b461e72ddfd80b0ce2fc26a59
Laura <laurazembrzuska@gmail.com>
2022-05-12 17:34:29 +0200

    Heatmap fixes

--- heatmap.py
+++ heatmap.py
@@ -9,9 +9,10 @@ class Heatmap:
         self.train_data = train_data
 
     def heatmap(self):
-        matrix = self.train_data.corr().round(2)
-        mask = np.triu(np.ones_like(matrix, dtype=bool))
-        sns.heatmap(matrix, annot=True, vmax=1, vmin=-1, center=0, cmap='vlag', mask=mask)
-        plt.show()
-        sns.clustermap(self.train_data, annot=True, center=0, cmap='vlag')
+        # matrix = self.train_data.corr().round(2)
+        # mask = np.triu(np.ones_like(matrix, dtype=bool))
+        # sns.heatmap(matrix, annot=True, vmax=1, vmin=-1, center=0, cmap='vlag', mask=mask)
+        # plt.show()
+        # sns.clustermap(self.train_data, annot=True, center=0, cmap='vlag')
+        sns.clustermap(self.train_data)
         plt.show()
