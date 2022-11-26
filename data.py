bedbcb6a2be5eea5ecd60fae5460f892e6faa654
Michał Sieczczyński <michal.siecz04@gmail.com>
2022-11-26 19:49:09 +0100

    Adjust autoencoder metrics, so it can be compared with other models

--- data.py
+++ data.py
@@ -13,42 +13,37 @@ class Data:
         self.train_indices = None
         self.test_indices = None
 
-        self.data = pd.read_csv(data_filepath, sep='\t')
+        self.data = pd.read_csv(data_filepath, sep="\t")
         self.data = self.data.transpose()
-        self.true_results = pd.read_csv(true_results_filepath, sep='\t')
+        self.true_results = pd.read_csv(true_results_filepath, sep="\t")
         self.data_amount = len(self.data.index)
 
-        cancer_indices = np.where(self.true_results == 1)[0]
-        healthy_indices = np.where(self.true_results == 0)[0]
-        self.healthy_data = self.data.iloc[healthy_indices]
-        self.cancer_data = self.data.iloc[cancer_indices]
+        self.cancer_train_data = None
+        self.cancer_test_data = None
         self.healthy_train_data = None
         self.healthy_test_data = None
-        self.healthy_train_indices = None
-        self.healthy_test_indices = None
-        self.healthy_data_amount = len(self.healthy_data.index)
 
     def generate_train_test_split(self):
         indices = np.arange(self.data_amount)
-        self.train_data, self.test_data, self.train_true_results, self.test_true_results, self.train_indices, \
-        self.test_indices = train_test_split(self.data,
-                                             self.true_results,
-                                             indices,
-                                             test_size=0.3,
-                                             random_state=42,
-                                             shuffle=True,
-                                             stratify=self.true_results)
+        (
+            self.train_data,
+            self.test_data,
+            self.train_true_results,
+            self.test_true_results,
+            self.train_indices,
+            self.test_indices,
+        ) = train_test_split(
+            self.data,
+            self.true_results,
+            indices,
+            test_size=0.3,
+            random_state=42,
+            shuffle=True,
+            stratify=self.true_results,
+        )
 
         np.save("train_indices.npy", self.train_indices)
         np.save("test_indices.npy", self.test_indices)
-
-    def generate_train_test_healthy_data_split(self):
-        indices = np.arange(self.healthy_data_amount)
-        self.healthy_train_data, self.healthy_test_data, self.healthy_train_indices, self.healthy_test_indices = \
-            train_test_split(self.healthy_data, indices, test_size=0.3, random_state=42, shuffle=True)
-
-        np.save("healthy_train_indices.npy", self.healthy_train_indices)
-        np.save("healthy_test_indices.npy", self.healthy_test_indices)
 
     def load_train_test_split(self, train_indices_filepath, test_indices_filepath):
         self.train_indices = np.load(train_indices_filepath)
@@ -60,22 +55,39 @@ class Data:
         self.train_true_results = self.true_results.iloc[self.train_indices]
         self.test_true_results = self.true_results.iloc[self.test_indices]
 
-        return self.train_data, self.test_data, self.train_true_results, self.test_true_results
-
-    def get_train_test_healthy_split_cancer_data(self, healthy_train_indices_filepath, healthy_test_indices_filepath):
-        self.healthy_train_indices = np.load(healthy_train_indices_filepath)
-        self.healthy_test_indices = np.load(healthy_test_indices_filepath)
-
-        self.healthy_train_data = self.healthy_data.iloc[self.healthy_train_indices]
-        self.healthy_test_data = self.healthy_data.iloc[self.healthy_test_indices]
-
-        return self.healthy_train_data, self.healthy_test_data, self.cancer_data
+        return (
+            self.train_data,
+            self.test_data,
+            self.train_true_results,
+            self.test_true_results,
+        )
 
     def get_converted_train_test_split(self):
-        return np.array(self.train_data, np.float32), \
-               np.array(self.test_data, np.float32), \
-               np.array(self.train_true_results, np.int32), \
-               np.array(self.test_true_results, np.int32)
+        return (
+            np.array(self.train_data, np.float32),
+            np.array(self.test_data, np.float32),
+            np.array(self.train_true_results, np.int32),
+            np.array(self.test_true_results, np.int32),
+        )
+
+    def load_train_test_healthy_cancer_data(self):
+        cancer_train_indices = np.where(self.train_true_results == 1)[0]
+        healthy_train_indices = np.where(self.train_true_results == 0)[0]
+
+        cancer_test_indices = np.where(self.test_true_results == 1)[0]
+        healthy_test_indices = np.where(self.test_true_results == 0)[0]
+
+        self.cancer_train_data = self.train_data.iloc[cancer_train_indices]
+        self.cancer_test_data = self.test_data.iloc[cancer_test_indices]
+        self.healthy_train_data = self.train_data.iloc[healthy_train_indices]
+        self.healthy_test_data = self.test_data.iloc[healthy_test_indices]
+
+        return (
+            self.healthy_train_data,
+            self.cancer_train_data,
+            self.healthy_test_data,
+            self.cancer_test_data,
+        )
 
     def get_all_data(self):
         return self.data
@@ -85,26 +97,73 @@ class Data:
 
     def get_scaled_train_test_data(self):
         scaler = StandardScaler().fit(self.train_data)
-        scaled_train_data = pd.DataFrame(scaler.transform(self.train_data), columns=self.train_data.columns)
-        scaled_test_data = pd.DataFrame(scaler.transform(self.test_data), columns=self.test_data.columns)
+        scaled_train_data = pd.DataFrame(
+            scaler.transform(self.train_data), columns=self.train_data.columns
+        )
+        scaled_test_data = pd.DataFrame(
+            scaler.transform(self.test_data), columns=self.test_data.columns
+        )
         return scaled_train_data, scaled_test_data
 
     def get_cut_by_max_train_test_data(self, threshold):
         columns_bool_values = self.train_data.max() > threshold
-        cut_by_max_train_data = self.train_data[self.train_data.columns[columns_bool_values]]
-        cut_by_max_test_data = self.test_data[self.test_data.columns[columns_bool_values]]
+        cut_by_max_train_data = self.train_data[
+            self.train_data.columns[columns_bool_values]
+        ]
+        cut_by_max_test_data = self.test_data[
+            self.test_data.columns[columns_bool_values]
+        ]
         return cut_by_max_train_data, cut_by_max_test_data
 
-    def get_scaled_healthy_train_test_cancer_data(self):
+    def scaled_data_by_healthy_train(self, data):
         scaler = StandardScaler().fit(self.healthy_train_data)
-        scaled_healthy_train_data = pd.DataFrame(scaler.transform(self.healthy_train_data), columns=self.healthy_train_data.columns)
-        scaled_healthy_test_data = pd.DataFrame(scaler.transform(self.healthy_test_data), columns=self.healthy_test_data.columns)
-        scaled_cancer_data = pd.DataFrame(scaler.transform(self.cancer_data), columns=self.cancer_data.columns)
-        return scaled_healthy_train_data, scaled_healthy_test_data, scaled_cancer_data
+        scaled_data = pd.DataFrame(
+            scaler.transform(data),
+            columns=data.columns,
+        )
+        return scaled_data
+
+    def get_scaled_healthy_train_test_cancer_data(self):
+        scaled_healthy_train_data = self.scaled_data_by_healthy_train(
+            self.healthy_train_data
+        )
+        scaled_healthy_test_data = self.scaled_data_by_healthy_train(
+            self.healthy_test_data
+        )
+        scaled_cancer_train_data = self.scaled_data_by_healthy_train(
+            self.cancer_train_data
+        )
+        scaled_cancer_test_data = self.scaled_data_by_healthy_train(
+            self.cancer_test_data
+        )
+        return (
+            scaled_healthy_train_data,
+            scaled_healthy_test_data,
+            scaled_cancer_train_data,
+            scaled_cancer_test_data,
+        )
+
+    def cut_by_max_healthy_data(self, data, threshold):
+        columns_bool_values = self.healthy_train_data.max() > threshold
+        cut_by_max_data = data[data.columns[columns_bool_values]]
+        return cut_by_max_data
 
     def get_cut_by_max_healthy_train_test_cancer_data(self, threshold):
-        columns_bool_values = self.healthy_train_data.max() > threshold
-        cut_by_max_healthy_train_data = self.healthy_train_data[self.healthy_train_data.columns[columns_bool_values]]
-        cut_by_max_healthy_test_data = self.healthy_test_data[self.healthy_test_data.columns[columns_bool_values]]
-        cut_by_max_cancer_data = self.cancer_data[self.cancer_data.columns[columns_bool_values]]
-        return cut_by_max_healthy_train_data, cut_by_max_healthy_test_data, cut_by_max_cancer_data
+        cut_by_max_healthy_train_data = self.cut_by_max_healthy_data(
+            self.healthy_train_data, threshold
+        )
+        cut_by_max_healthy_test_data = self.cut_by_max_healthy_data(
+            self.healthy_test_data, threshold
+        )
+        cut_by_max_cancer_train_data = self.cut_by_max_healthy_data(
+            self.cancer_train_data, threshold
+        )
+        cut_by_max_cancer_test_data = self.cut_by_max_healthy_data(
+            self.cancer_test_data, threshold
+        )
+        return (
+            cut_by_max_healthy_train_data,
+            cut_by_max_healthy_test_data,
+            cut_by_max_cancer_train_data,
+            cut_by_max_cancer_test_data,
+        )
