Fix AUC calculation; Add features test; Check other datasets (#19)

simple_ml_models.py
@@ -1,5 +1,11 @@
 from sklearn.model_selection import cross_validate
-from sklearn.metrics import balanced_accuracy_score, f1_score, precision_score, recall_score, roc_auc_score
+from sklearn.metrics import (
+    balanced_accuracy_score,
+    f1_score,
+    precision_score,
+    recall_score,
+    roc_auc_score,
+)
 import numpy as np
 import pandas as pd
 
@@ -7,12 +13,21 @@ from utils import get_best_metrics
 
 
 class Model:
-    def __init__(self, model, train_data, train_true_results, test_data, test_true_results, fold_number,
-                 minus_one_one_values=False):
+    def __init__(
+        self,
+        model,
+        train_data,
+        train_true_results,
+        test_data,
+        test_true_results,
+        fold_number,
+        minus_one_one_values=False,
+    ):
         self.model = model
         self.train_data = train_data
         self.fold_number = fold_number
         self.test_data = test_data
+        self.minus_one_one_values = minus_one_one_values
 
         if minus_one_one_values:
             self.test_true_results = test_true_results.replace([0, 1], [1, -1])
@@ -21,7 +36,7 @@ class Model:
             self.test_true_results = test_true_results
             self.train_true_results = train_true_results
 
-        self.scoring = ['balanced_accuracy', 'roc_auc', 'precision', 'recall', 'f1']
+        self.scoring = ["balanced_accuracy", "roc_auc", "precision", "recall", "f1"]
         self.test_balanced_accuracy_list = []
         self.test_roc_auc_list = []
         self.test_f1_score_list = []
@@ -29,30 +44,51 @@ class Model:
         self.test_precision_list = []
 
     def main_cycle(self):
-        cv_results = cross_validate(self.model, self.train_data, self.train_true_results,
-                                    cv=self.fold_number, return_train_score=True, scoring=self.scoring,
-                                    return_estimator=True)
+        cv_results = cross_validate(
+            self.model,
+            self.train_data,
+            self.train_true_results,
+            cv=self.fold_number,
+            return_train_score=True,
+            scoring=self.scoring,
+            return_estimator=True,
+        )
         # Model.display_metrics(cv_results)
 
-        for cross_validation_model in cv_results['estimator']:
+        for cross_validation_model in cv_results["estimator"]:
             self.gather_test_results(cross_validation_model)
 
     def gather_test_results(self, model):
         predictions = model.predict(self.test_data)
 
-        test_balanced_accuracy = balanced_accuracy_score(self.test_true_results, predictions)
+        test_balanced_accuracy = balanced_accuracy_score(
+            self.test_true_results, predictions
+        )
         self.test_balanced_accuracy_list.append(test_balanced_accuracy)
 
-        test_roc_auc = roc_auc_score(self.test_true_results, predictions)
-        self.test_roc_auc_list.append(test_roc_auc)
+        if self.minus_one_one_values:
+            scores = model.decision_function(self.test_data)
+            test_roc_auc = roc_auc_score(self.test_true_results, scores)
+            self.test_roc_auc_list.append(test_roc_auc)
+        else:
+            test_roc_auc = roc_auc_score(
+                self.test_true_results, model.predict_proba(self.test_data)[::, 1]
+            )
+            self.test_roc_auc_list.append(test_roc_auc)
 
-        test_precision = precision_score(self.test_true_results, predictions, average='weighted')
+        test_precision = precision_score(
+            self.test_true_results, predictions, average="weighted"
+        )
         self.test_precision_list.append(test_precision)
 
-        test_recall = recall_score(self.test_true_results, predictions, average='weighted')
+        test_recall = recall_score(
+            self.test_true_results, predictions, average="weighted"
+        )
         self.test_recall_list.append(test_recall)
 
-        test_f1_score = f1_score(self.test_true_results, predictions, average='weighted')
+        test_f1_score = f1_score(
+            self.test_true_results, predictions, average="weighted"
+        )
         self.test_f1_score_list.append(test_f1_score)
 
     def display_test_results(self):
@@ -74,18 +110,25 @@ class Model:
     def display_metrics(cv_results):
         for value in cv_results.keys():
             str_value = str(value)
-            if str_value.startswith('test_'):
-                str_value = str_value.replace('test', "Validation")
-                str_value = str_value.replace('_', ' ')
+            if str_value.startswith("test_"):
+                str_value = str_value.replace("test", "Validation")
+                str_value = str_value.replace("_", " ")
                 print(str_value, cv_results[value].mean())
-            if str_value.startswith('train_'):
-                str_value = str_value.replace('train', "Training")
-                str_value = str_value.replace('_', ' ')
+            if str_value.startswith("train_"):
+                str_value = str_value.replace("train", "Training")
+                str_value = str_value.replace("_", " ")
                 print(str_value, cv_results[value].mean())
                 print()
 
 
-def run_all_models(classifiers, clfs_names, train_data, train_true_results, test_data, test_true_results):
+def run_all_models(
+    classifiers,
+    clfs_names,
+    train_data,
+    train_true_results,
+    test_data,
+    test_true_results,
+):
     balanced_acc = []
     roc_auc = []
     precision = []
@@ -98,9 +141,15 @@ def run_all_models(classifiers, clfs_names, train_data, train_true_results, test
         if clf_name == "Isolated Forest":
             minus_one_one_values = True
 
-        model_object = Model(model=clf, train_data=train_data, train_true_results=train_true_results,
-                             test_data=test_data, test_true_results=test_true_results, fold_number=3,
-                             minus_one_one_values=minus_one_one_values)
+        model_object = Model(
+            model=clf,
+            train_data=train_data,
+            train_true_results=train_true_results,
+            test_data=test_data,
+            test_true_results=test_true_results,
+            fold_number=3,
+            minus_one_one_values=minus_one_one_values,
+        )
         model_object.main_cycle()
 
         balanced_acc.append(np.mean(model_object.test_balanced_accuracy_list))
@@ -109,15 +158,22 @@ def run_all_models(classifiers, clfs_names, train_data, train_true_results, test
         recall.append(np.mean(model_object.test_recall_list))
         f1.append(np.mean(model_object.test_f1_score_list))
 
-    data = {"Balanced Accuracy": balanced_acc, "ROC AUC": roc_auc, "Precision": precision, "Recall": recall,
-            "F1 score": f1}
+    data = {
+        "Balanced Accuracy": balanced_acc,
+        "ROC AUC": roc_auc,
+        "Precision": precision,
+        "Recall": recall,
+        "F1 score": f1,
+    }
     results = pd.DataFrame(data=data, index=clfs_names)
 
-    print("Best balanced accuracy:", ', '.join(get_best_metrics(clfs_names, balanced_acc)))
-    print("Best ROC AUC:", ', '.join(get_best_metrics(clfs_names, roc_auc)))
-    print("Best precision:", ', '.join(get_best_metrics(clfs_names, precision)))
-    print("Best recall:", ', '.join(get_best_metrics(clfs_names, recall)))
-    print("Best F1 score:", ', '.join(get_best_metrics(clfs_names, f1)))
+    print(f"Features number: {len(train_data.columns)}")
+    print(
+        "Best balanced accuracy:", ", ".join(get_best_metrics(clfs_names, balanced_acc))
+    )
+    print("Best ROC AUC:", ", ".join(get_best_metrics(clfs_names, roc_auc)))
+    print("Best precision:", ", ".join(get_best_metrics(clfs_names, precision)))
+    print("Best recall:", ", ".join(get_best_metrics(clfs_names, recall)))
+    print("Best F1 score:", ", ".join(get_best_metrics(clfs_names, f1)))
 
     return results
-

utils.py
@@ -9,12 +9,14 @@ from sklearn.feature_selection import mutual_info_classif
 
 
 def draw_dendrogram(data, linkage_method, p):
-    plt.figure(figsize=(15,8))
-    dendro = sch.dendrogram(sch.linkage(data, method=linkage_method), truncate_mode='lastp', p=p)
-    plt.ylabel(f'kryterium łączenia klastrów ({linkage_method})',fontsize=15)
-    plt.title(f'dendrogram dla kryterium: {linkage_method}',fontsize=15)
+    plt.figure(figsize=(15, 8))
+    dendro = sch.dendrogram(
+        sch.linkage(data, method=linkage_method), truncate_mode="lastp", p=p
+    )
+    plt.ylabel(f"kryterium łączenia klastrów ({linkage_method})", fontsize=15)
+    plt.title(f"dendrogram dla kryterium: {linkage_method}", fontsize=15)
     plt.show
-    
+
 
 def draw_heatmap(data):
     sns.clustermap(data)
@@ -28,7 +30,10 @@ def calculate_statistics(data, true_results):
     healthy_cells_data = data.iloc[healthy_cells_indices]
     cancer_cells_data = data.iloc[cancer_cells_indices]
 
-    d = {'ctc_variance': np.var(cancer_cells_data), 'other_cells_variance': np.var(healthy_cells_data)}
+    d = {
+        "ctc_variance": np.var(cancer_cells_data),
+        "other_cells_variance": np.var(healthy_cells_data),
+    }
     statistics = pd.DataFrame(data=d)
 
     statistics["ctc_mean"] = np.mean(cancer_cells_data, axis=0)
@@ -37,22 +42,42 @@ def calculate_statistics(data, true_results):
     statistics["ctc_std"] = np.std(cancer_cells_data)
     statistics["other_cells_std"] = np.std(healthy_cells_data)
 
-    variance_check_1 = statistics['ctc_variance']/statistics['other_cells_variance'] < 4
-    variance_check_2 = statistics['other_cells_variance']/statistics['ctc_variance'] < 4
+    variance_check_1 = (
+        statistics["ctc_variance"] / statistics["other_cells_variance"] < 4
+    )
+    variance_check_2 = (
+        statistics["other_cells_variance"] / statistics["ctc_variance"] < 4
+    )
     statistics["variance_check"] = variance_check_1 & variance_check_2
 
-    t_statistics = np.where(statistics["variance_check"], stats.ttest_ind(healthy_cells_data, cancer_cells_data, equal_var = True).statistic, stats.ttest_ind(healthy_cells_data, cancer_cells_data, equal_var = False).statistic)
+    t_statistics = np.where(
+        statistics["variance_check"],
+        stats.ttest_ind(
+            healthy_cells_data, cancer_cells_data, equal_var=True
+        ).statistic,
+        stats.ttest_ind(
+            healthy_cells_data, cancer_cells_data, equal_var=False
+        ).statistic,
+    )
     t_statistics = np.absolute(t_statistics)
-    statistics['t_test'] = t_statistics
+    statistics["t_test"] = t_statistics
 
-    p_values = np.where(statistics["variance_check"], stats.ttest_ind(healthy_cells_data, cancer_cells_data, equal_var = True).pvalue, stats.ttest_ind(healthy_cells_data, cancer_cells_data, equal_var = False).pvalue)
-    statistics['p_values'] = p_values
-    statistics["information_gain"] = mutual_info_classif(data, true_results, random_state=42)
+    p_values = np.where(
+        statistics["variance_check"],
+        stats.ttest_ind(healthy_cells_data, cancer_cells_data, equal_var=True).pvalue,
+        stats.ttest_ind(healthy_cells_data, cancer_cells_data, equal_var=False).pvalue,
+    )
+    statistics["p_values"] = p_values
+    statistics["information_gain"] = mutual_info_classif(
+        data, true_results, random_state=42
+    )
 
     return statistics.sort_values(by="p_values")
 
 
-def display_neural_network_metrics(model, batch_size_value, data, true_results, set_name):
+def display_neural_network_metrics(
+    model, batch_size_value, data, true_results, set_name
+):
     results = model.evaluate(data, true_results, batch_size=batch_size_value)
 
     print(f"{set_name} loss: {results[0]}")
@@ -65,24 +90,28 @@ def display_neural_network_metrics(model, batch_size_value, data, true_results, 
 def get_best_shap_features(shap_results, feature_names, amount):
     rf_result_x = pd.DataFrame(shap_results[0], columns=feature_names)
     vals = np.abs(rf_result_x.values).mean(0)
-    shap_importance = pd.DataFrame(list(zip(feature_names, vals)),
-                                   columns=['col_name', 'feature_importance_vals'])
-    shap_importance.sort_values(by=['feature_importance_vals'],
-                                ascending=False, inplace=True)
-    best_features = list(shap_importance.head(amount)['col_name'])
+    shap_importance = pd.DataFrame(
+        list(zip(feature_names, vals)), columns=["col_name", "feature_importance_vals"]
+    )
+    shap_importance.sort_values(
+        by=["feature_importance_vals"], ascending=False, inplace=True
+    )
+    best_features = list(shap_importance.head(amount)["col_name"])
     return best_features
 
 
 def get_best_metrics(clfs_names, metric):
-    return [clfs_names[x] for x in [idx for idx, x in enumerate(metric) if x == max(metric)]]
+    return [
+        clfs_names[x] for x in [idx for idx, x in enumerate(metric) if x == max(metric)]
+    ]
 
 
 def variance_between(d1, d2):
     d = pd.concat([d1, d2], axis=0)
-    
-    p1 = len(d1)/len(d)
-    p2 = len(d2)/len(d)
-    
-    var = (p1*p2)*pow((d1.mean()-d2.mean()), 2)
-    
+
+    p1 = len(d1) / len(d)
+    p2 = len(d2) / len(d)
+
+    var = (p1 * p2) * pow((d1.mean() - d2.mean()), 2)
+
     return var